571793d6de206a5d3bbf87766efa862ccbbf2d79
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-05 16:22:49 +0200

    add form inputs to create view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
     <div class="col-sm-8 blog-main">
-        <h1>Create a Post</h1>
+        <h1>Publish a Post</h1>
+        <hr>
+        
+        <form method="POST" action="/posts">
+            <div class="form-group">
+                <label for="title" class="form-label">Title:</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+
+            <div class="form-group">
+                <label for="body" class="form-label">Body</label>
+                <textarea name="body" id="body" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Publish</button>
+                </div>
+            </div>
+        </form>
     </div>
-@endsection
+        
+        @endsection
