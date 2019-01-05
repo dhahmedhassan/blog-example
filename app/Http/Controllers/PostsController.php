e002015f7c900595f0696ef0b3350f6c8c995159
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-05 18:33:25 +0200

    add store function in PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -19,5 +20,16 @@ class PostsController extends Controller
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        
+        Post::create(request(['title', 'body']));
+
+        // Redirects to somewhere - Home Page -
+
+        return redirect('/');
+
     }
 }
