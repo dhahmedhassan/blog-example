652f271d88bc1c12e41416a4747f1e161e055401
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-06 16:13:08 +0200

    fix the guarded array in Model.php model

--- app/Model.php
+++ app/Model.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model as Elequent;
 class Model extends Elequent
 {
     // protected $fillable = ['title', 'body']; // these are only the fields that are okey to mass assign - white listed -
-    protected $guarded = ['user_id']; // these are the fields that you do not want to allow - blacklisted - // you can leave it empty and in this case you're not guarding anything.
+    protected $guarded = []; // these are the fields that you do not want to allow - blacklisted - // you can leave it empty and in this case you're not guarding anything.
 }
