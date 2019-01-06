d6f255d23f389af83e6f09e56c64305353b62bab
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-06 15:53:44 +0200

    include the msgs partial in create & show views

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -24,15 +24,7 @@
             </div>
         </form>
 
-        @if (count($errors->all()) > 0)
-            <div class="alert alert-danger form-group">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li> {{$error}} </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include('layouts.msgs')
     </div>
         
         @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -32,13 +32,14 @@
                 <form method="POST" action="/posts/{{$post->id}}/comments">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <textarea name="body" placeholder="Your Comment Here!" class="form-control"></textarea>
+                        <textarea name="body" placeholder="Your Comment Here!" class="form-control" required=""/></textarea>
                     </div>
                         
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Add Comment</button>
                     </div>
                 </form>
+                @include('layouts.msgs')
             </div>
         </div>
 
