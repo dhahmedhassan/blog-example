f2673cc809a9d5a76a1aab3b7117a8cf974bc13a
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-07 22:25:50 +0200

    add store function in RegisterationController

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class RegistrationController extends Controller
 {
     public function create()
     {
         return view('sessions.create');
+    }
+
+    public function store() 
+    {
+        // validate the form
+        $this->validate(request(), [
+            'name' => 'required|max:25',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        // create and save the user.
+        $user = User::create(request(['name', 'email', 'password']));
+        // Sign in
+        auth()->login($user);
+        // redirect
+        return redirect()->home();
     }
 }
