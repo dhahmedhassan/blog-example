e083c61176539d9a5461891dafdb764de8201fd7
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-08 18:33:45 +0200

    add construct, store and fix redirect in SessionsController

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,15 +6,34 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'destroy']);
+    }
     public function create()
     {
         return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // Attempt to authenticate the user.
+        if (! auth()->attempt(request(['email', 'password'])))
+        {
+            return back()->withErrors([
+                'message' => 'Please check your Credentials and try again.'
+            ]);
+        }
+
+        // Redirect to the home page
+        return redirect()->home();
     }
 
     public function destroy()
     {
         auth()->logout();
 
-        redirect()->home();
+        return redirect()->home();
     }
 }
