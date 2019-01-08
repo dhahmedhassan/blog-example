36c593aea612094c3bcaaabe2051a0b2b73b3027
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-08 18:35:57 +0200

    change the sessions.create to login form

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,40 +1,28 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="col-sm-8">
-        <h1>Register</h1>
-            <form action="/register" method="post">
-                {{ csrf_field() }}
-                <div class="form-group">
-                  <label for="name">Name </label>
-                  <input type="text"
-                    class="form-control" name="name" id="name" aria-describedby="helpName" placeholder="Your Name">
-                  <small id="helpName" class="form-text text-muted">Please Enter your First and Last name!</small>
-                </div>
+    <div class="col-md-8">
+        <h1 class="text-center">Sign In</h1>
+        <form method="POST" action="/login">
+            {{ csrf_field() }}
+            <div class="form-group">
+              <label for="email">Email Address</label>
+              <input type="email"
+                class="form-control" name="email" id="email" aria-describedby="helpEmail" placeholder="email@example.com">
+              <small id="helpEmail" class="form-text text-muted">Please Enter your Email Address</small>
+            </div>
 
-                <div class="form-group">
-                  <label for="email">Email</label>
-                  <input type="email"
-                    class="form-control" name="email" id="email" aria-describedby="helpEmail" placeholder="example@email.com">
-                  <small id="helpEmail" class="form-text text-muted">Please Enter Your Email!</small>
-                </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" name="password" id="password" placeholder="">
+            </div>
 
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" name="password" id="password" placeholder="">
-                </div>
+            <div class="form-group">
+                <button type="submit" name="btn" id="btn" class="btn btn-primary" btn-lg btn-block">Login</button>
+            </div>
 
-                <div class="form-group">
-                  <label for="password_confirmation">Password Confirmation</label>
-                  <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="">
-                </div>
-
-                <div class="form-group">
-                  <button type="submit" class="btn btn-primary">Register</button>
-                </div>
-                @include('layouts.msgs') 
-
-            </form>
-
+            @include('layouts.msgs')
+            
+        </form>
     </div>
 @endsection
