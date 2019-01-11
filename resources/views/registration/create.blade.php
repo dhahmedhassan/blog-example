6b85af451d5c50d338cd8ca4a19c2be400fac9a7
Mohamed Mostapha <mohamed.mostapha3699@gmail.com>
2019-01-10 16:03:35 -0800

    add return input fields with old values registeration.create

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -8,14 +8,14 @@
                 <div class="form-group">
                   <label for="name">Name </label>
                   <input type="text"
-                    class="form-control" name="name" id="name" aria-describedby="helpName" placeholder="Your Name">
+                    class="form-control" name="name" id="name" aria-describedby="helpName" placeholder="Your Name" value="{{ Request::old('name')}}">
                   <small id="helpName" class="form-text text-muted">Please Enter your First and Last name!</small>
                 </div>
 
                 <div class="form-group">
                   <label for="email">Email</label>
                   <input type="email"
-                    class="form-control" name="email" id="email" aria-describedby="helpEmail" placeholder="example@email.com">
+                    class="form-control" name="email" id="email" aria-describedby="helpEmail" placeholder="example@email.com" value="{{ Request::old('email')}}">
                   <small id="helpEmail" class="form-text text-muted">Please Enter Your Email!</small>
                 </div>
 
